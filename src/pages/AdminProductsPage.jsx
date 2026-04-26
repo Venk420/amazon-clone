7e153ceee25db8456da67f222e9011c1f8de5906
Venk420 <75911121+Venk420@users.noreply.github.com>
2026-04-26 14:14:53 +0400

    Update AdminProductsPage.jsx

--- src/pages/AdminProductsPage.jsx
+++ src/pages/AdminProductsPage.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { useProducts } from "../context2/ProductsContext.jsx";
+import { useProducts } from "../context2/productsContext.jsx";
 
 export default function AdminProductsPage() {
   const { products } = useProducts();
