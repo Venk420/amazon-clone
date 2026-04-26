fcf57aa77b9fe0c0b5a6e404d4570229b808331d
Venk420 <75911121+Venk420@users.noreply.github.com>
2026-04-26 14:15:08 +0400

    Update AdminEditProductPage.jsx

--- src/pages/AdminEditProductPage.jsx
+++ src/pages/AdminEditProductPage.jsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom";
-import { useProducts } from "../context2/ProductsContext.jsx";
+import { useProducts } from "../context2/productsContext.jsx";
 
 export default function AdminEditProductPage() {
   const { id } = useParams();
