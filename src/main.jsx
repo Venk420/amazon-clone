a5abce63be72a3bba99217dad776e2ea221f0707
Venk420 <75911121+Venk420@users.noreply.github.com>
2026-04-26 13:22:26 +0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-import { AuthProvider } from "./context/AuthContext.jsx";
-import { CartProvider } from "./context/CartContext.jsx";
-import { ProductsProvider } from "./context/ProductsContext.jsx";
+import { AuthProvider } from "./context2/AuthContext.jsx";
+import { CartProvider } from "./context2/CartContext.jsx";
+import { ProductsProvider } from "./context2/ProductsContext.jsx";
 import "./styles/globals.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
