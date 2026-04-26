5b83f1a2dd77ad01e978270c1a834cf93b43992d
Venk420 <75911121+Venk420@users.noreply.github.com>
2026-04-26 14:01:10 +0400

    Create ProtectedRoute.jsx

---
+++ src/components/ProtectedRoute.jsx
@@ -0,0 +1,16 @@
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../context2/AuthContext.jsx";
+
+export default function ProtectedRoute({ children, requireAdmin }) {
+  const { user } = useAuth();
+
+  if (!user) {
+    return <Navigate to="/login" replace />;
+  }
+
+  if (requireAdmin && !user.isAdmin) {
+    return <Navigate to="/" replace />;
+  }
+
+  return children;
+}
